b1e65c61a6ee693734c28172ad1f17221b172a9b
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-09 20:16:39 -0500

    Basic Movement Complete

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float force;
+    [SerializeField] private float gravity;
+
+    private Rigidbody physics;
+
+    void Awake()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        physics.AddForce(new Vector3(x * force, -gravity, y * force));
+    }
+}
