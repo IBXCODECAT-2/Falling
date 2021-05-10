dd5f3ccb475b0a95c45f139d0ae7f1fea9c0e427
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-09 20:27:54 -0500

    Score System Added

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private float updateSpeed;
+    [SerializeField] private TextMeshProUGUI score;
+    [SerializeField] private Transform player;
+
+    private IEnumerator Start()
+    {
+        while(true)
+        {
+            score.text = "Depth: " + player.position.y.ToString();
+            yield return new WaitForSeconds(updateSpeed);
+        }
+    }
+}
