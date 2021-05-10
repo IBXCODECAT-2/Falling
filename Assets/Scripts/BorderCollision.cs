abb2bbf1b910ba51bcd721d496f17a2d6eda31f6
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-10 18:24:11 -0500

    Introduced Block Destruction Mechanic

---
+++ Assets/Scripts/BorderCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCollision : MonoBehaviour
+{
+    [SerializeField] private GameObject[] audioObjects;
+    [SerializeField] private GameObject particleObject;
+    [SerializeField] private Transform player;
+    [SerializeField] private float destroyTime;
+
+    void OnCollisionEnter(Collision other)
+    {
+        int random = Random.Range(0, audioObjects.Length);
+
+        Instantiate(audioObjects[random], player.position, Quaternion.identity, null);
+        Destroy(audioObjects[random], destroyTime);
+
+        Instantiate(particleObject, player.position, Quaternion.identity, null);
+        Destroy(particleObject, destroyTime);
+    }
+}
---
+++ Assets/Scripts/CubeExplosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeExplosion : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionObject;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Instantiate(explosionObject, transform.position, Quaternion.identity, null);
+        Destroy(gameObject);
+    }
+}
