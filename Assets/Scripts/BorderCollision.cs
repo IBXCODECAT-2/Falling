f691159b5d807d0825ed357a49b5e9257c8416fb
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-11 17:44:18 -0500

    Enhanced Destroy Effect

--- Assets/Scripts/BorderCollision.cs
+++ Assets/Scripts/BorderCollision.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class BorderCollision : MonoBehaviour
 {
     [SerializeField] private GameObject[] audioObjects;
-    [SerializeField] private GameObject particleObject;
     [SerializeField] private Transform player;
-    [SerializeField] private float destroyTime;
 
     void OnCollisionEnter(Collision other)
     {
         int random = Random.Range(0, audioObjects.Length);
 
         Instantiate(audioObjects[random], player.position, Quaternion.identity, null);
-
-        Instantiate(particleObject, player.position, Quaternion.identity, null);
     }
 }
