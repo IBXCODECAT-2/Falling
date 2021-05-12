c77d203bb18b3de96971af652c509a3f4bd66a54
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-11 20:09:33 -0500

    Continued to work on tutorial thing

--- Assets/Scripts/TutorialInfo.cs
+++ Assets/Scripts/TutorialInfo.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System.Collections;
 
 public class TutorialInfo : MonoBehaviour
 {
     [SerializeField] private GameObject infoObject;
     [SerializeField] private TextMeshProUGUI info;
+    [SerializeField] private Button continueButton;
     [SerializeField] private string[] tutorialMessages;
     [SerializeField] private bool[] paused;
+    [SerializeField] private float automatedContinueDelay;
 
     private short msgCount = 0;
     private void Awake()
     {
         Time.timeScale = 0f;
+    }
+
+    private IEnumerator Delay(short msg)
+    {
+        switch(msg)
+        {
+            case 2:
+                yield return new WaitForSecondsRealtime(automatedContinueDelay);
+                break;
+            default:
+                yield return new WaitForSecondsRealtime(automatedContinueDelay * 2);
+                break;
+        }
+
+        NextMessage();
     }
 
     public void NextMessage()
@@ -27,12 +46,13 @@ public class TutorialInfo : MonoBehaviour
         if(paused[msgCount])
         {
             Time.timeScale = 0f;
-            infoObject.SetActive(true);
+            continueButton.enabled = true;
         }
         else
         {
             Time.timeScale = 1f;
-            infoObject.SetActive(false);
+            continueButton.enabled = false;
+            StartCoroutine(Delay(msgCount));
         }
 
         msgCount++;
