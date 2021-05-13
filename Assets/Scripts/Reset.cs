fd48f5c81518b1d46933ce1cca3f940c9c4c5115
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-12 19:54:31 -0500

    Add Level Reload Debug Script

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    [SerializeField] private GameObject load;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            load.SetActive(true);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
