ff6d5707d8111c684516bdd21f67a1422b88727f
IBXCODECAT <nathan.schmitt7@gmail.com>
2021-05-16 19:52:14 -0500

    Lost scene, level loader script commitable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
+    [Header("Level Loading")]
+    [SerializeField] private int[] scenes;
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private TextMeshProUGUI progressReport;
+
     private void Awake()
     {
-
         DontDestroyOnLoad(gameObject);
-
+        
         if (instance == null)
         {
-            instance = this;
+            Init();
         }
         else
         {
@@ -23,11 +30,24 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        
+
     }
 
-    private void Update()
+    private void Init()
     {
-        
+        instance = this;
+
+        progressBar.maxValue = 0.9f * scenes.Length;
+
+        foreach (int scene in scenes)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+
+            while(!operation.isDone)
+            {
+                progressBar.value = operation.progress;
+                //progressReport.text = "Loading:" + operation.ToString();
+            }
+        }
     }
 }
